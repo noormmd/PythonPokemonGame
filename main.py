d49193b213935af5894a3a387ca521a515ce4af7
noormmd <noormamdouh004@gmail.com>
2025-09-10 13:04:33 +0100

    Added functionality to generate a random pokemon with id and display details

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import random
 
 # Get the list of pokemon from the API
 url = 'https://pokeapi.co/api/v2/pokemon/'
@@ -36,3 +37,31 @@ print('Name: {}'.format(pokemon_data['name']))
 print('Weight: {}'.format(weight_formatted) + "(kgs)")
 print('Height: {}'.format(height_formatted) + "(m)")
 print('Ability: {}'.format(ability['name']))
+
+#Computer generated pokemon
+print("You will play against the computer")
+
+# Generate a random pokemon by ID
+random_id = random.randint(1, 1302)
+
+# Get request to retrieve pokemon details with random id
+pokemon2 = requests.get(f'https://pokeapi.co/api/v2/pokemon/{random_id}/').json()
+
+# Assign values from API
+pokemon2_name = pokemon2['name']
+pokemon2_ability = pokemon2['abilities'][0]['ability']['name']
+pokemon2_height = pokemon2['height'] / 10
+pokemon2_weight = pokemon2['weight'] / 10
+
+# Display pokemon details from API
+print("\nComputer's Pokémon:")
+print(f"Name: {pokemon2_name.capitalize()}")
+print(f"Ability: {pokemon2_ability.capitalize()}")
+print(f"Height: {pokemon2_height} m")
+print(f"Weight: {pokemon2_weight} kg")
+
+#Start of game prompt
+print("Start game? (y/n)")
+choice = input().lower()
+#if choice == 'y':
+   # print("")
